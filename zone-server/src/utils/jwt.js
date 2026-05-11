e3e7f0dfbc876a176d05924b333a08a3179fd136
Soumojit-Makar <ssoumojitmakar1234@gmail.com>
2026-05-11 18:47:32 +0530

    push1.11

--- zone-server/src/utils/jwt.js
+++ zone-server/src/utils/jwt.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const env = require("../config/env");
+const bcrypt = require("bcryptjs");
 
 const signToken = (payload) => {
   return jwt.sign(payload, env.JWT_SECRET, { expiresIn: env.JWT_EXPIRES_IN });
